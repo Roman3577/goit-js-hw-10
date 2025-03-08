1dccce958a891cfae1e1a97cc117dc90607c32ad
Roma3577 <mularr37@gmail.com>
2025-03-08 16:51:08 +0200

    task2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,43 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const input = document.querySelector("[required]");
+const form = document.querySelector(".form"); 
+const delayInput = document.querySelector("input[name='delay']"); 
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const delay = Number(delayInput.value); 
+    const selectedState = document.querySelector("input[name='state']:checked")?.value;
+
+    if (!delay) {
+        iziToast.warning({
+            title: "Caution",
+            message: "You forgot important data",
+            position: "topRight",
+            timeout: 5000,
+        });
+        return; 
+    }
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (selectedState === "fulfilled") {
+                resolve(`✅ Fulfilled promise in ${delay}ms`);
+            } else {
+                reject(`❌ Rejected promise in ${delay}ms`);
+            }
+        }, delay);
+    });
+    promise
+        .then(value => iziToast.success({
+            title: "OK",
+            message: value,
+            position: "topRight",
+            timeout: 5000,
+        }))
+        .catch(error => iziToast.error({
+            title: "Error",
+            message: error,
+            position: "topRight",
+            timeout: 5000,
+        }));
+});
